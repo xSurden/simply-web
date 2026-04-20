5ea80fb261b9c25846d15554833e65a96437cbad
xSurden <hi@surden.me>
2026-04-20 17:58:50 +0100

    Testing Git without desktop app (on linux)

---
+++ src/Modules/Users/Account.php
@@ -0,0 +1,23 @@
+<?php
+
+
+    namespace App\Modules\Users;
+
+    class Account {
+
+
+        /*
+        Ensure that all of the required table exists
+        */
+
+        /*
+        Public methods such as: login, register, getSession, logout
+        */
+
+
+        // Get session
+        public function getSession() {
+            if (!isset())
+        }
+
+    }
