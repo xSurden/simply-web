f62f29160d6a5b5a7a0fe8d29bfe197db30959cc
xSurden <hi@surden.me>
2026-04-24 08:25:36 +0100

    Added view via $Views->load(view_name, dependencies);

---
+++ src/Server/Handle/Views.php
@@ -0,0 +1,35 @@
+<?php
+
+
+    namespace App\Server\Handle;
+
+    use Exception;
+
+    class Views {
+
+        private $default_route = ABSPATH . "/resources/views/";
+
+        public function load($view_name = null, $dependencies = []) {
+            if ($view_name === null) {
+                throw new Exception("Unable to load view: Not provided");
+            }
+
+            $path_built = $this->default_route . $view_name;
+
+            if (file_exists($path_built)) {
+                if (!empty($dependencies)) {
+                    extract($dependencies);
+
+                    // Ensure dependencies exist -- may limit performance
+                    $Dependencies = new \App\Server\Dependencies();
+                    extract($Dependencies->fetch());
+                    ob_start();
+                }
+                include $path_built . ".php";
+                return true;
+            }
+
+            throw new Exception("Unable to load view: view file not found");
+        }
+
+    }
