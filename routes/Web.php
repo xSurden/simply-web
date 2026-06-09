607e3d52ab1d9b4f5935312ec7f17104ae348053
xSurden <hi@surden.me>
2026-06-09 08:51:29 +0100

    Update Web.php

--- routes/Web.php
+++ routes/Web.php
@@ -1,5 +1,10 @@
 <?php
 
+    // Get the Router class
     use App\Blueprint\Web\Router;
 
+    // Defined Routes below
+    /*
+    Example: Router::get("/example", "app/demo/example");
+    */
     Router::get('/', 'home');
